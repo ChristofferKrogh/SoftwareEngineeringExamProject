b886eb53fc8ba0d5a6456b9ae9cec89134f34e9c
Thina Lundsgaard Thøgersen <s165541@student.dtu.dk>
2019-03-25 15:58:13 +0100

    Create employee

--- src/dtu/planning/app/Employee.java
+++ src/dtu/planning/app/Employee.java
@@ -2,4 +2,22 @@ package dtu.planning.app;
 
 public class Employee {
 
+	private String name; 
+	private String initials; 
+	
+	public Employee(String name, String initials) {
+		this.name = name; 
+		this.initials = initials; 
+	}
+	
+	public String getName() {
+		return name; 
+	}
+	
+	public String getInitials() {
+		return initials; 
+	}
+	
+	
+	
 }
