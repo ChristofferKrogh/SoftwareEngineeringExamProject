43039ceb1003b2662ceceb776407938d0105c33b
Clara Chini Nielsen <s164213@student.dtu.dk>
2019-04-08 16:42:52 +0200

    Started working on: correct_reported_time

--- test/dtu/planning/acceptance_tests/CorrectReportedTimeSteps.java
+++ test/dtu/planning/acceptance_tests/CorrectReportedTimeSteps.java
@@ -14,32 +14,44 @@ import dtu.planning.app.TimeRegistration;
 public class CorrectReportedTimeSteps {
 	
 	// "Global" variable holders so steps can be used across features
-		private PlanningAppHolder planningAppHolder;
-		private ProjectHolder projectHolder;
-		private EmployeeHolder employeeHolder;
-		private ErrorMessageHolder errorMessageHolder;
-		private ActorHolder actorHolder;
-		
-		// Private variables, will give problems when otheres need to use them. Create holder then?
-		private TimeRegistration timeRegistration;
+	private PlanningAppHolder planningAppHolder;
+	private ProjectHolder projectHolder;
+	private EmployeeHolder employeeHolder;
+	private ErrorMessageHolder errorMessageHolder;
+	private ActorHolder actorHolder;
+	
+	// Private variables, will give problems when otheres need to use them. Create holder then?
+	private TimeRegistration timeRegistration;
 
-		public CorrectReportedTimeSteps(PlanningAppHolder planningAppHolder, ErrorMessageHolder errorMessageHolder, ProjectHolder projectHolder, EmployeeHolder employeeHolder, ActorHolder actorHolder) {
-			this.planningAppHolder = planningAppHolder;
-			this.errorMessageHolder = errorMessageHolder;
-			this.projectHolder = projectHolder;
-			this.employeeHolder = employeeHolder;
-			this.actorHolder = actorHolder;
-		}
-		
+	public CorrectReportedTimeSteps(PlanningAppHolder planningAppHolder, ErrorMessageHolder errorMessageHolder, ProjectHolder projectHolder, EmployeeHolder employeeHolder, ActorHolder actorHolder) {
+		this.planningAppHolder = planningAppHolder;
+		this.errorMessageHolder = errorMessageHolder;
+		this.projectHolder = projectHolder;
+		this.employeeHolder = employeeHolder;
+		this.actorHolder = actorHolder;
+	}
+	
 
-	@When("I updatede time used")
-	public void iSendUpdatedTimeUsed() {
+	@Given("the employee with initials {string} has reported time for the activity with name {string}")
+	public void theEmployeeWithInitialsHasReportedTimeForTheActivityWithName(String string, String string2) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new cucumber.api.PendingException();
 	}
 
-	@Then("the time is corrected in the time report")
-	public void theTimeIsCorrectedInTheTimeReport() {
+	@When("I updatede time used by adding {int} hours")
+	public void iUpdatedeTimeUsedByAddingHours(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("the updated time report is saved to activity with name {string}")
+	public void theUpdatedTimeReportIsSavedToActivityWithName(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("the employee with initials {string} does not have reported time for the activity with name {string}")
+	public void theEmployeeWithInitialsDoesNotHaveReportedTimeForTheActivityWithName(String string, String string2) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new cucumber.api.PendingException();
 	}
