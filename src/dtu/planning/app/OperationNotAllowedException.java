8abaa8fc1705f766d9f8383b150d8f6c7888b607
Emil Kristensen <eKristensen@users.noreply.github.com>
2019-05-05 20:29:17 +0200

    Down to 15 warnings

--- src/dtu/planning/app/OperationNotAllowedException.java
+++ src/dtu/planning/app/OperationNotAllowedException.java
@@ -3,6 +3,11 @@ package dtu.planning.app;
 public class OperationNotAllowedException extends Exception {
 
 	/**
+	 * Generated a default serial version unique ID
+	 */
+	private static final long serialVersionUID = -6797101480481250870L;
+
+	/**
 	 * A new exception is constructed with error message errorMessage.
 	 * @param errorMessage the error message of the exception
 	 */
